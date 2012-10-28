ab66ddcf4ca9569747110ba498bab4f66d3d3787
Adam DeRusha <apd3691@gmail.com>
2012-10-28 18:10:08 -0400

    Added an is_tutor() method.

--- RPI_tutor_time/tutor_time/models.py
+++ RPI_tutor_time/tutor_time/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your models here.
 class Tutee(models.Model):
     user = models.OneToOneField(User)
+
+    def is_tutor(self):
+        try:
+            self.tutor
+        except Tutor.DoesNotExist:
+            return False
+        return True
 
     # Place other fields here such as
     # phone_number = models.PhoneNumberField() 
