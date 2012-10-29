f5c2711bd124fb276020934b78d7fe1b6da142ec
Sheena McNeil <mcneis@rpi.edu>
2012-10-28 21:34:01 -0400

    added requests into models

--- RPI_tutor_time/tutor_time/models.py
+++ RPI_tutor_time/tutor_time/models.py
@@ -11,3 +11,29 @@ class Tutee(models.Model):
 
 class Tutor(Tutee):
     pass
+
+
+class Request(models.Model):
+    request_by = models.CharField(max_length=30)
+    accepted_by = models.CharField(max_length=30, null=True)
+    for_class = models.CharField(max_length=30)
+    description = models.CharField(max_length=300, null=True)
+    MON = 'M'
+    TUES = 'T'
+    WED = 'W'
+    THURS = 'R'
+    FRI = 'F'
+    SAT = 'S'
+    SUN = 'U'
+    DAYS_CHOICES = (
+        (MON, 'Mon'),
+        (TUES, 'Tues'),
+        (WED, 'Wed'),
+        (THURS, 'Thurs'),
+        (FRI, 'Fri'),
+        (SAT, 'Sat'),
+        (SUN, 'Sun')
+    )
+    days = models.CharField(max_length=2, choices=DAYS_CHOICES)
+    time = models.TimeField()
+
