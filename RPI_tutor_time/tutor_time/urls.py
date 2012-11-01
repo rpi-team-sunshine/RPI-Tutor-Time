5e77d91415367ec41aa4ec8130d1913fa9b10fa7
Zhenzheng <zhouz3@rpi.edu>
2012-10-31 21:49:34 -0400

    request tutor page

--- RPI_tutor_time/tutor_time/urls.py
+++ RPI_tutor_time/tutor_time/urls.py
@@ -1,9 +1,28 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls.defaults import patterns, include, url
 
-from tutor_time import views
+
+import tutor_time.views
+import django.contrib.auth.views
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^claim_tutee/', views.claim_tutee, name='claim_tutee'),
-    url(r'^create_account/', views.create_account, name='create account'),
-    url(r'^$', views.index, name='index'),
+    # Examples:
+    # url(r'^$', 'RPI_tutor_time.views.home', name='home'),
+    # url(r'^RPI_tutor_time/', include('RPI_tutor_time.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #url(r'^create_account/', include('tutor_time.urls')),
+    url(r'^claim_tutee/', tutor_time.views.claim_tutee, name='claim_tutee'),
+    url(r'^create_account/', tutor_time.views.create_account),
+    url(r'^request_tutor/', tutor_time.views.request_tutor),
+    url(r'^login/', django.contrib.auth.views.login),
+    url(r'^logout/', tutor_time.views.logout_view),
+    url(r'^$', tutor_time.views.index),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
 )
--- RPI_tutor_time/tutor_time/views.py
+++ RPI_tutor_time/tutor_time/views.py
@@ -5,7 +5,7 @@ from django.template import RequestContext
 from django.core.context_processors import csrf
 from django.contrib.auth.models import User
 from django.contrib.auth import logout 
-from tutor_time.models import Tutee, Tutor
+from tutor_time.models import Tutee, Tutor, Request
 from tutor_time.utility import *
 
 def index(request):
@@ -13,26 +13,47 @@ def index(request):
     context.update(csrf(request))
     return render_to_response('index.html', context)
 
+def request_tutor(request):
+    first_name = request.POST['first_name'];	
+    last_name = request.POST['last_name'];
+    for_class = request.POST['for_class'];
+    description = request.POST['description'];
+
+    #Request = Tutee.objects.request_tutor(username,email,password)
+    r = Request    
+    r.first_name = first_name
+    r.last_name = last_name
+    r.for_class = for_class
+    r.description = description
+    r.save()
+
+    return render_to_response('index.html',
+                                  context_instance=RequestContext(request, c))
+
 def create_account(request):
     c = {
         'password_error': '',
         'username_error': '',
-        'email_error': ''
+        'email_error': '',
+        'firstname_error': '',
+        'lastname_error': ''
     }
 
     if request.method == 'POST':
+        valid = validate_creation(request.POST)
+        if valid is not None:
+            c.update(valid)
+            c.update(csrf(request))
+            return render_to_response('create_account.html',
+                                      context_instance=RequestContext(request, c))
+
         username = request.POST['username']
         fname = request.POST['fname']
         lname = request.POST['lname']
         email = request.POST['email']
         password = request.POST['password']
         password_confirm = request.POST['pwconfirm']
-        valid = validate_creation(username, password, password_confirm, email)
-        if valid is not None:
-            c.update(valid)
-            c.update(csrf(request))
-            return render_to_response('create_account.html',
-                                      context_instance=RequestContext(request, c))
+
         useracct = User.objects.create_user(username,email,password)
         useracct.first_name = fname
         useracct.last_name = lname
@@ -57,5 +78,7 @@ def logout_view(request):
 def claim_tutee(request):
     tutee_list = Tutee.objects.all()
     return render_to_response('claim_tutee.html', {'tutee_list': tutee_list})
+
+
 
 
