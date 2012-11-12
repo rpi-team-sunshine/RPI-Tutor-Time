3c44a4cfc1c0f5850e5f307c4a26c31ee9bd07b5
Adam DeRusha <apd3691@gmail.com>
2012-11-11 21:33:21 -0500

    Added a simulate email method since emails are not sending.

--- RPI_tutor_time/tutor_time/emails.py
+++ RPI_tutor_time/tutor_time/emails.py
@@ -20,4 +20,19 @@ class emails():
         payload['From'] = self.ouremail
         payload['To'] = user.email
         payload['Subject'] = subject 
+        print self.ouremail
+        print user.email
+        print payload.as_string()
         self.smtp.sendmail(self.ouremail, [user.email], payload.as_string())
+
+    def simulate_send(self, user, msg, subject):
+        f = open(subject.replace(' ','') + '.html','w')
+        payload = MIMEText(msg)
+        payload['From'] = self.ouremail
+        payload['To'] = user.email
+        payload['Subject'] = subject 
+        print self.ouremail
+        print user.email
+        print payload.as_string()
+        f.write(payload.as_string())
+        f.close()
